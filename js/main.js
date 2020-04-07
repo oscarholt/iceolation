b72f31a0e7b99afde8e844aaee2cf8908f825dbd
Clive Cooper <yaclive@gmail.com>
2020-04-07 15:46:40 +1200

    updated template

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-function setup() {
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
 
+var player = {
+	x: 0,
+	y: 0,
+	xV: 0,
+	yV: 0,
+	xA: 0,
+	yA: 0,
+	init: function() {
+		app.stage.addChild(this.sprite);
+	},
+	update: function() {
+		this.xV += this.xA;
+		this.yV += this.yA;
+
+		this.x += this.xV;
+		this.y += this.yV;
+	},
+	sprite: PIXI.Sprite.from('img/player.jpg')
 }
 
-function draw() {
-	var i = 50;
-	var j = 80;
-	var count = 0;
-	while(count < 10){
-		ellipse(i, i, j, j);
-		i += 10;
-		j += 10;
-		count++;
-	}
+function init() {
+	player.init();
 }
+
+app.ticker.add((delta) => { // Draw
+	player.update();
+});
+
+init();
